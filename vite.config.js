f22806a63883c6b1e0993d55bc8df68febab1a24
Hedge1001 <136144006+Hedge1001@users.noreply.github.com>
2026-08-03 21:51:03 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
-import { defineConfig } from sb-2w8b360mtstd.vercel.r
+import react from sb-2w8b360mtstd.vercel.r
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
+  server: {"sb-3iiqvt8fwd9p.vercel.run"
     port: 5173,
   },
 })

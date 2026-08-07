e4074f81c6ef8ba3a3087c0992b6d3c7d7638dec
Hedge1001 <136144006+Hedge1001@users.noreply.github.com>
2026-08-07 12:48:20 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
-import { defineConfig } from "vite";server.allowedHostssb-mb0gl56258wp.vercel.run
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: [
+      "sb-3104rd0i36qi.vercel.run",
+    ],
+  },
 });

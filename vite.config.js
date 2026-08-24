ddb8da9b6555526bb8351be194263c9655697c22
Hedge1001 <136144006+Hedge1001@users.noreply.github.com>
2026-08-23 23:15:18 -0500

    Remove allowedHosts from Vite server configuration
    Removed allowedHosts configuration from server settings.

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,4 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: {sb-603oko2wjcev.vercel.run
-    allowedHosts: [add "sb-3104rd0i36qi.vercel.run" to server
-      "sb-3104rd0i36qi.vercel.run",
-    ],
-  },
 });

12b9260f8e978689bb715efb88eee4e7af0c71ea
Hedge1001 <136144006+Hedge1001@users.noreply.github.com>
2026-08-26 08:45:07 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,20 +2,3 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
-import './index.css';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
-);
-
-if ('serviceWorker' in navigator && import.meta.env.PROD) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js').catch((err) => {
-      console.warn('HoopCast: service worker registration failed.', err);
-    });
-  });
-}
